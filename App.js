6e30b1659abaf74b2f0c1ffd7ae5faf70285f5df
Frank Chau <frank.chau@wellintech.com.hk>
2018-05-23 18:36:00 +0800

    delete all first

--- App.js
+++
@@ -1,9 +0,0 @@
-import Reactotron from 'reactotron-react-native'
-Reactotron
-  .configure() // controls connection & communication settings
-  .useReactNative() // add all built-in react native plugins
-  .connect() // let's connect!
-
-import App from './app/pages/router.js';
-
-export default App;
