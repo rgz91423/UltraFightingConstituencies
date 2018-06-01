57a038697d648a9d1d60cef24bff13411c8facce
Frank Chau <frank.chau@wellintech.com.hk>
2018-06-01 18:43:55 +0800

    use nativebase

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-import Reactotron from 'reactotron-react-native'
-Reactotron
-  .configure() // controls connection & communication settings
-  .useReactNative() // add all built-in react native plugins
-  .connect() // let's connect!
-
 import App from './app/pages/router.js';
 
 import { YellowBox } from 'react-native';
